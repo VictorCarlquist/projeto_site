9fd4b376b9bfa8f4495c61fc1e334b1515ebb68e
GAugusto <gaugustomacedo@gmail.com>
2014-08-19 12:38:41 -0300

    Efeito ou Defeito.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 document.onscroll = function () {
-	var menu = document.getElementById("menu");
+	var menu = document.getElementById("menu_box");
 	if(window.pageYOffset > 10){
-		menu.style.left = "-1000px";
+		menu.style.background = "linear-gradient(#111, #333, #111)";
+		menu.style.visibility = "hidden";
+		menu.style.opacity = "0";
+		menu.style.transition = "visibility 0s linear 0.5s,opacity 0.5s linear";
 	}else{
-		menu.style.left = "0px";
+		menu.style.background = "linear-gradient(#333, #111, #333)";
+		menu.style.visibility = "visible";
+		menu.style.opacity = "1";
+		menu.style.transitionDelay = "0s";
 	}
 }
