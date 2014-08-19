b641d78d0692093a5be0706cfc7aa87845fa8831
Victor Carlquist <victorcarlquist@gmail.com>
2014-08-19 19:30:32 -0300

    novo estilo

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+var isTop = true;
+var menu = document.getElementById("menu");
+
+menu.onmouseover = function (){
+	if(!isTop)
+		menu.style.left = "-400px";
+}
+
+menu.onmouseout = function (){
+	if(!isTop)
+		menu.style.left = "-1300px";
+}
+
 document.onscroll = function () {
-	var menu = document.getElementById("menu");
 	if(window.pageYOffset > 10){
-		menu.style.left = "-1000px";
+		menu.style.left = "-1300px";
+		isTop = false;
 	}else{
 		menu.style.left = "0px";
+		isTop = true;
 	}
 }
